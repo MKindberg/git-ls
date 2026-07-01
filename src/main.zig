29aaabec4d58187f9fdc06e70c21688a2b7c9f65
Marcus <6180175+MKindberg@users.noreply.github.com>
2026-07-01 23:51:01 +0200

    Add lint command

--- src/main.zig
+++ src/main.zig
@@ -15,6 +15,7 @@ fn createConfigMap(allocator: std.mem.Allocator, io: std.Io) !ConfigMap {
     }
     var lines = std.mem.splitScalar(u8, git_result.stdout, '\n');
     while (lines.next()) |line| {
+        if (line.len == 0) break; // The list ends with an empty line
         var parts = std.mem.splitScalar(u8, line, '.');
 
         const p1 = parts.next().?;
@@ -33,11 +34,100 @@ fn createConfigMap(allocator: std.mem.Allocator, io: std.Io) !ConfigMap {
     return configs;
 }
 
+fn hasConfig(configs: ConfigMap, level1: []const u8, level2: ?[]const u8, level3: ?[]const u8) bool {
+    var buf: [64]u8 = undefined;
+    var buf2: [64]u8 = undefined;
+    var l1_keys = configs.keyIterator();
+    const l2 = l2: while (l1_keys.next()) |k| {
+        if (std.mem.eql(u8, std.ascii.lowerString(&buf, level1), std.ascii.lowerString(&buf2, k.*))) break :l2 configs.get(k.*).?;
+    } else return false;
+
+    if (level2 == null) return true;
+    var l2_keys = l2.keyIterator();
+    while (l2_keys.next()) |k| {
+        if (std.mem.eql(u8, std.ascii.lowerString(&buf, level2.?), std.ascii.lowerString(&buf2, k.*)) or k.*[0] == '<' or k.*[0] == '*') {
+            if (level3 == null) return true;
+            const l3 = l2.get(k.*).?;
+            var l3_keys = l3.keyIterator();
+            while (l3_keys.next()) |kk| {
+                if (std.mem.eql(u8, std.ascii.lowerString(&buf, level3.?), std.ascii.lowerString(&buf, kk.*)) or kk.*[0] == '<' or k.*[0] == '*') return true;
+            }
+        }
+    }
+    return false;
+}
+
+fn err(file: []const u8, line: usize, comptime message: []const u8, args: anytype) noreturn {
+    std.debug.print("{s}:{} " ++ message ++ "\n", .{ file, line } ++ args);
+    std.process.exit(1);
+}
+
 pub fn main(init: std.process.Init) !u8 {
     const configs = try createConfigMap(init.arena.allocator(), init.io);
-    var keys = configs.keyIterator();
-    while (keys.next()) |k| {
-        std.debug.print("{s}\n", .{k.*});
+
+    var args = init.minimal.args.iterate();
+    _ = args.skip();
+    const command = args.next() orelse return 1;
+    const file = args.next() orelse return 1;
+    const content = try std.Io.Dir.cwd().readFileAlloc(init.io, file, init.gpa, .unlimited);
+    defer init.gpa.free(content);
+
+    if (std.mem.eql(u8, command, "lint")) {
+        var lines = std.mem.splitScalar(u8, content, '\n');
+        var level1: ?[]const u8 = null;
+        var level2: ?[]const u8 = null;
+
+        var l: usize = 1;
+        while (lines.next()) |line| : (l += 1) {
+            var c: usize = 0;
+            while (c < line.len and std.ascii.isWhitespace(line[c])) c += 1;
+            // Parse section
+            if (c == line.len) continue;
+            if (line[c] == '#' or line[c] == ';') continue;
+            if (line[c] == '[') {
+                level1 = null;
+                level2 = null;
+                const section_start = c + 1;
+                const section_end = std.mem.findAnyPos(u8, line, c, ". ]") orelse err(file, l, "Missing end bracket", .{});
+                level1 = line[section_start..section_end];
+                if (!hasConfig(configs, level1.?, null, null)) err(file, l, "Invalid section {s}", .{level1.?});
+                c = section_end;
+                while (c < line.len and std.ascii.isWhitespace(line[c])) c += 1;
+                if (line[c] == '.') {
+                    const subsection_start = c + 1;
+                    const subsection_end = std.mem.findAnyPos(u8, line, c, " ]") orelse err(file, l, "Missing end bracket", .{});
+                    level2 = line[subsection_start..subsection_end];
+                    if (!hasConfig(configs, level1.?, level2.?, null)) err(file, l, "Invalid subsection {s}", .{level2.?});
+                    c = subsection_end;
+                }
+                if (line[c] == '"') {
+                    c += 1;
+                    const subsection_start = c;
+                    const subsection_end = std.mem.findScalarPos(u8, line, c, '"') orelse err(file, l, "Missing end quote", .{});
+                    level2 = line[subsection_start..subsection_end];
+                    if (!hasConfig(configs, level1.?, level2.?, null)) err(file, l, "Invalid subsection {s}", .{level2.?});
+                    c = subsection_end + 1;
+                }
+                while (c < line.len and std.ascii.isWhitespace(line[c])) c += 1;
+                if (line[c] != ']') err(file, l, "Missing end bracket", .{});
+                c += 1;
+                while (c < line.len and std.ascii.isWhitespace(line[c])) c += 1;
+                if (c != line.len and c != '#') err(file, l, "Text after bracket no allowed", .{});
+                continue;
+            } else {
+                if (level1 == null) err(file, l, "Everything needs to be part of a section", .{});
+                const option_start = c;
+                const option_end = std.mem.findAnyPos(u8, line, c, "= ") orelse line.len;
+                if (level2 == null) {
+                    const key = line[option_start..option_end];
+                    if (!hasConfig(configs, level1.?, key, null)) err(file, l, "Invalid key '{s}'", .{key});
+                } else {
+                    const key = line[option_start..option_end];
+                    if (!hasConfig(configs, level1.?, level2.?, key)) err(file, l, "Invalid key '{s}'", .{key});
+                }
+            }
+        }
     }
+
     return 0;
 }
