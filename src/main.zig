1ede77c0d1d509926d142c5c6e61c5e716d82892
Marcus <6180175+MKindberg@users.noreply.github.com>
2026-07-21 21:23:02 +0200

    Move tree-sitter logic into babel

--- src/main.zig
+++ src/main.zig
@@ -8,22 +8,12 @@ const lint = @import("lint.zig");
 const State = struct {
     config_text: []const u8,
     configs: ConfigMap,
-    language: *ts.Language,
-    parser: *ts.Parser,
-    tree: *ts.Tree,
 
     const Self = @This();
-    fn init(config_text: []const u8, configs: ConfigMap, content: []const u8) Self {
-        const language = tree_sitter_git_config();
-        const parser = ts.Parser.create();
-        parser.setLanguage(language) catch unreachable;
-        const tree = parser.parseString(content, null) orelse unreachable;
+    fn init(config_text: []const u8, configs: ConfigMap) Self {
         return State{
             .config_text = config_text,
             .configs = configs,
-            .language = language,
-            .parser = parser,
-            .tree = tree,
         };
     }
 
@@ -32,13 +22,10 @@ const State = struct {
     }
 
     fn deinit(self: Self) void {
-        self.tree.destroy();
-        const language = self.parser.getLanguage().?;
-        self.parser.destroy();
-        language.destroy();
+        _ = self;
     }
 };
-const Lsp = lsp.Lsp(.{ .state_type = State, .update_doc_on_change = false });
+const Lsp = lsp.Lsp(.{ .state_type = State, .document_type = lsp.TreeSitterDocument });
 
 extern fn tree_sitter_git_config() callconv(.c) *ts.Language;
 const Io = std.Io;
@@ -128,8 +115,9 @@ fn getConfigText(allocator: std.mem.Allocator, io: std.Io) ![]const u8 {
 fn handleOpenDoc(p: Lsp.OpenDocumentParameters) void {
     const configs = createConfigMap(p.gpa, p.io) catch unreachable;
     const config_text = getConfigText(p.gpa, p.io) catch unreachable;
-    p.context.state = State.init(config_text, configs, p.context.document.text);
-    sendDiagnostics(p.allocator, p.context.server, &p.context.state.?, p.context.document);
+    p.context.state = State.init(config_text, configs);
+    p.context.document.init_ts(tree_sitter_git_config()) catch unreachable;
+    sendDiagnostics(p.arena.allocator(), p.context.server, &p.context.state.?, p.context.document);
 }
 
 fn handleCloseDoc(p: Lsp.CloseDocumentParameters) void {
@@ -141,27 +129,8 @@ fn posToPoint(p: lsp.types.Position) ts.Point {
     return .{ .row = @intCast(p.line), .column = @intCast(p.character) };
 }
 fn handleChange(p: Lsp.ChangeDocumentParameters) void {
-    var doc: *lsp.Document = &p.context.document;
-    var tree: *ts.Tree = p.context.state.?.tree;
-    var parser: *ts.Parser = p.context.state.?.parser;
-    for (p.changes) |c| {
-        var edit = ts.InputEdit{
-            .start_byte = @intCast(doc.posToIdx(c.range.?.start).?),
-            .old_end_byte = @intCast(doc.posToIdx(c.range.?.end).?),
-            .start_point = posToPoint(c.range.?.start),
-            .old_end_point = posToPoint(c.range.?.end),
-            .new_end_byte = 0,
-            .new_end_point = .{ .row = 0, .column = 0 },
-        };
-        doc.update(c) catch unreachable;
-        edit.new_end_byte = @intCast(edit.start_byte + c.text.len);
-        edit.new_end_point = posToPoint(doc.idxToPos(edit.new_end_byte).?);
-
-        tree.edit(edit);
-        tree = parser.parseString(doc.text, tree).?;
-        p.context.state.?.tree = tree;
-    }
-    sendDiagnostics(p.allocator, p.context.server, &p.context.state.?, p.context.document);
+    const allocator = p.arena.allocator();
+    sendDiagnostics(allocator, p.context.server, &p.context.state.?, p.context.document);
 }
 
 fn handleSave(p: Lsp.SaveDocumentParameters) void {
@@ -170,11 +139,11 @@ fn handleSave(p: Lsp.SaveDocumentParameters) void {
     // sendDiagnostics(p.allocator, p.context.server, &state, p.context.document);
 }
 
-fn sendDiagnostics(allocator: std.mem.Allocator, server: *Lsp, state: *State, doc: lsp.Document) void {
+fn sendDiagnostics(allocator: std.mem.Allocator, server: *Lsp, state: *State, doc: Lsp.Document) void {
     var diagnostics = std.ArrayList(lsp.types.Diagnostic).empty;
 
-    var cursor = state.tree.walk();
-    const errors = lint.lint(allocator, state.configs, doc.text, &cursor) catch unreachable;
+    var cursor = doc.tree.?.walk();
+    const errors = lint.lint(allocator, state.configs, doc.doc.text, &cursor) catch unreachable;
 
     for (errors.items) |e| {
         diagnostics.append(allocator, .{
@@ -189,7 +158,7 @@ fn sendDiagnostics(allocator: std.mem.Allocator, server: *Lsp, state: *State, do
     }
 
     const d = lsp.types.Notification.PublishDiagnostics{ .params = .{
-        .uri = doc.uri,
+        .uri = doc.doc.uri,
         .diagnostics = diagnostics.items,
     } };
     server.writeResponse(allocator, d) catch unreachable;
@@ -197,9 +166,6 @@ fn sendDiagnostics(allocator: std.mem.Allocator, server: *Lsp, state: *State, do
 
 fn addText(allocator: std.mem.Allocator, text: *std.ArrayList(u8), new: []const u8) void {
     text.appendSlice(allocator, new) catch unreachable;
-}
-fn addNodeText(allocator: std.mem.Allocator, content: []const u8, text: *std.ArrayList(u8), node: ts.Node) void {
-    addText(allocator, text, content[node.startByte()..node.endByte()]);
 }
 fn nextNode(cursor: *ts.TreeCursor, skip_children: bool) ?ts.Node {
     if (!skip_children and cursor.gotoFirstChild()) return cursor.node();
@@ -210,14 +176,11 @@ fn nextNode(cursor: *ts.TreeCursor, skip_children: bool) ?ts.Node {
     return null;
 }
 fn handleFormat(p: Lsp.FormattingParameters) Lsp.FormattingReturn {
-    const allocator = p.allocator;
-    const state: State = p.context.state.?;
+    const allocator = p.arena.allocator();
     const doc = p.context.document;
-    var cursor = state.tree.walk();
+    var cursor = doc.tree.?.walk();
     var edits = std.ArrayList(lsp.types.TextEdit).empty;
-    var new_text = std.ArrayList(u8).initCapacity(p.allocator, p.context.document.text.len) catch unreachable;
-
-    const content = doc.text;
+    var new_text = std.ArrayList(u8).initCapacity(allocator, p.context.document.doc.text.len) catch unreachable;
 
     var skip_children = false;
     while (nextNode(&cursor, skip_children)) |node| {
@@ -227,22 +190,22 @@ fn handleFormat(p: Lsp.FormattingParameters) Lsp.FormattingReturn {
             if (prev.childCount() == 0) continue;
             if (std.mem.eql(u8, prev.child(prev.childCount() - 1).?.kind(), "comment")) continue;
             if (std.mem.eql(u8, (node.prevSibling() orelse continue).kind(), "section"))
-                addText(p.allocator, &new_text, "\n");
+                addText(allocator, &new_text, "\n");
         }
-        if (std.mem.eql(u8, node.kind(), "[")) addText(p.allocator, &new_text, "[");
+        if (std.mem.eql(u8, node.kind(), "[")) addText(allocator, &new_text, "[");
         if (std.mem.eql(u8, node.kind(), "section_name")) {
-            addNodeText(allocator, content, &new_text, node);
+            addText(allocator, &new_text, doc.nodeText(node));
             if (std.mem.eql(u8, (node.nextSibling() orelse continue).kind(), "\"")) addText(allocator, &new_text, " ");
         }
-        if (std.mem.eql(u8, node.kind(), "\"")) addText(p.allocator, &new_text, "\"");
-        if (std.mem.eql(u8, node.kind(), "subsection_name")) addNodeText(allocator, content, &new_text, node);
+        if (std.mem.eql(u8, node.kind(), "\"")) addText(allocator, &new_text, "\"");
+        if (std.mem.eql(u8, node.kind(), "subsection_name")) addText(allocator, &new_text, doc.nodeText(node));
         if (std.mem.eql(u8, node.kind(), "]")) {
             addText(allocator, &new_text, "]");
             if (node.nextSibling() == null or !std.mem.eql(u8, node.nextSibling().?.kind(), "comment"))
                 addText(allocator, &new_text, "\n");
         }
         if (std.mem.eql(u8, node.kind(), "variable")) addText(allocator, &new_text, "    ");
-        if (std.mem.eql(u8, node.kind(), "name")) addNodeText(allocator, content, &new_text, node);
+        if (std.mem.eql(u8, node.kind(), "name")) addText(allocator, &new_text, doc.nodeText(node));
         if (std.mem.eql(u8, node.kind(), "=")) {
             addText(allocator, &new_text, " = ");
         }
@@ -252,18 +215,18 @@ fn handleFormat(p: Lsp.FormattingParameters) Lsp.FormattingReturn {
             std.mem.eql(u8, node.kind(), "false"))
         {
             skip_children = true;
-            addNodeText(allocator, content, &new_text, node);
+            addText(allocator, &new_text, doc.nodeText(node));
             if (node.nextSibling() == null or !std.mem.eql(u8, node.nextSibling().?.kind(), "comment"))
                 addText(allocator, &new_text, "\n");
         }
         if (std.mem.eql(u8, node.kind(), "comment")) {
             if (node.nextSibling() == null and node.parent().?.nextSibling() != null) {
-                addText(p.allocator, &new_text, "\n");
+                addText(allocator, &new_text, "\n");
             } else if (node.prevSibling() != null) {
-                addText(p.allocator, &new_text, "    ");
+                addText(allocator, &new_text, "    ");
             }
-            addNodeText(allocator, content, &new_text, node);
-            addText(p.allocator, &new_text, "\n");
+            addText(allocator, &new_text, doc.nodeText(node));
+            addText(allocator, &new_text, "\n");
         }
     }
 
@@ -271,7 +234,7 @@ fn handleFormat(p: Lsp.FormattingParameters) Lsp.FormattingReturn {
         .newText = std.mem.trim(u8, new_text.items, &std.ascii.whitespace),
         .range = .{
             .start = .{ .line = 0, .character = 0 },
-            .end = doc.idxToPos(doc.text.len - 1).?,
+            .end = doc.doc.idxToPos(doc.doc.text.len - 1).?,
         },
     }) catch unreachable;
     return edits.items;
@@ -308,9 +271,9 @@ fn lookupDocumentation(config_text: []const u8, section_name: []const u8, subsec
 }
 fn handleHover(p: Lsp.HoverParameters) Lsp.HoverReturn {
     const state: State = p.context.state.?;
-    const root = state.tree.rootNode();
+    const root = p.context.document.tree.?.rootNode();
 
-    const content = p.context.document.text;
+    const content = p.context.document.doc.text;
 
     const point: ts.Point = .{ .row = @intCast(p.position.line), .column = @intCast(p.position.character) };
     const node = root.namedDescendantForPointRange(point, point) orelse return null;
